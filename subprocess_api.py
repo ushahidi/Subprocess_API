ad4c3dd6a7fe8b30a85f33f8ac4e0959c34ee968
Charl van Niekerk <charl@ushahidi.com>
2011-12-18 14:52:22 +0100

    Organised imports according to alphabetical order.

--- subprocess_api.py
+++ subprocess_api.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from json import dumps, loads
 from subprocess import call
 from tempfile import TemporaryFile
