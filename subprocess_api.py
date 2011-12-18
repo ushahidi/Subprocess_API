71495a877b5053c94e250058586c122538b8d860
Charl van Niekerk <charl@ushahidi.com>
2011-12-18 14:52:45 +0100

    Converted tabs to spaces.

--- subprocess_api.py
+++ subprocess_api.py
@@ -20,39 +20,39 @@ from tempfile import TemporaryFile
 from traceback import format_exc
 
 class HTTPRequestHandler(BaseHTTPRequestHandler):
-	def do_POST(self):
-		try:
-			request = loads(self.rfile.read(int(self.headers['Content-Length'])))
+    def do_POST(self):
+        try:
+            request = loads(self.rfile.read(int(self.headers['Content-Length'])))
 
-			if 'stdin' in request:
-				stdin = TemporaryFile()
-				stdin.write(request['stdin'])
-				stdin.seek(0)
-			else:
-				stdin = None
+            if 'stdin' in request:
+                stdin = TemporaryFile()
+                stdin.write(request['stdin'])
+                stdin.seek(0)
+            else:
+                stdin = None
 
-			stdout = TemporaryFile()
-			stderr = TemporaryFile()
+            stdout = TemporaryFile()
+            stderr = TemporaryFile()
 
-			shell = request['shell'] if 'shell' in request else False
+            shell = request['shell'] if 'shell' in request else False
 
-			returncode = call(request['args'], stdin=stdin, stdout=stdout, stderr=stderr, shell=shell)
+            returncode = call(request['args'], stdin=stdin, stdout=stdout, stderr=stderr, shell=shell)
 
-			stdout.seek(0)
-			stderr.seek(0)
+            stdout.seek(0)
+            stderr.seek(0)
 
-			response_code = 200
-			response_body = {'returncode': returncode, 'stdout': stdout.read(), 'stderr': stderr.read()}
-		except:
-			response_code = 500
-			response_body = {'error': format_exc()}
+            response_code = 200
+            response_body = {'returncode': returncode, 'stdout': stdout.read(), 'stderr': stderr.read()}
+        except:
+            response_code = 500
+            response_body = {'error': format_exc()}
 
-		self.send_response(response_code)
-		self.send_header('Content-Type', 'application/json')
-		self.end_headers()
+        self.send_response(response_code)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
 
-		self.wfile.write(dumps(response_body))
-		self.wfile.write('\n')
+        self.wfile.write(dumps(response_body))
+        self.wfile.write('\n')
 
 server = HTTPServer(('localhost', 8000), HTTPRequestHandler)
 server.serve_forever()
